19cdcca135e0f1bae7c4fd1bdad4a97a79b56ce5
Julio Cesar Aparecido Gaiotto <julio.gaiotto@totvs.com.br>
2020-02-13 10:47:36 -0300

    importando arquivos de imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import profile from './assets/logo.png';
+
 function App() {
-  return <h1>React</h1>
+  return (
+    <>
+      <h1>React</h1>
+      <img width="150" src={profile} />
+    </>
+  )
 }
 
 export default App;
